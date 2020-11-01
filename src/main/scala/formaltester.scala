b5c4977976293db096d796fec450a4a15ecf23f0
Daniel Kasza <daniel@kasza.hu>
2020-11-01 14:20:03 -0500

    Improve the naming of test case directories.

--- src/main/scala/formaltester.scala
+++ src/main/scala/formaltester.scala
@@ -7,19 +7,19 @@ import scala.reflect._
 trait FormalTester { this: FlatSpec =>
     def cover[T<:FormalModule](gen: => T, depth: Int = 20)(implicit c: ClassTag[T]) {
         it must "be covered" in {
-            new SbyRun(gen, "cover", depth, this.suiteName).throwErrors()
+            new SbyRun(gen, "cover", depth, this.suiteName+"_").throwErrors()
         }
     }
 
     def prove[T<:FormalModule](gen: => T, depth: Int = 20)(implicit c: ClassTag[T]) {
         it must "be proven" in {
-            new SbyRun(gen, "prove", depth, this.suiteName).throwErrors()
+            new SbyRun(gen, "prove", depth, this.suiteName+"_").throwErrors()
         }
     }
 
     def bmc[T<:FormalModule](gen: => T, depth: Int = 20)(implicit c: ClassTag[T]) {
         it must ("pass bounded model check with depth " + depth.toString) in {
-            new SbyRun(gen, "bmc", depth, this.suiteName).throwErrors()
+            new SbyRun(gen, "bmc", depth, this.suiteName+"_").throwErrors()
         }
     }
 }
