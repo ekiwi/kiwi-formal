8c16b6133df7990d762852b3f455c43b859948d2
Kevin Läufer <laeufer@cs.berkeley.edu>
2021-01-11 18:43:55 -0800

    test: FSM assert/assume

--- test/kiwi/formal/StateMachineTest.scala
+++ test/kiwi/formal/StateMachineTest.scala
@@ -11,7 +11,6 @@ class SimpleFSM extends MultiIOModule {
   val b = IO(Output(Bool()))
   b := false.B
 
-  //val state = Reg(UInt(2.W))
   val state = RegInit(0.U(2.W))
 
   switch(state) {
@@ -48,6 +47,19 @@ class FSMStateSequenceCover(f: SimpleFSM) extends Spec(f) {
   visitAfter(2, 1)
   visitAfter(0, 2) // this is expected to fail since the FSM is stuck in state 2
 }
+class FSMInputOutputAssert(f: SimpleFSM) extends Spec(f) {
+  // a && b may never be true for two consecutive cycles
+  assert(!(past(f.a && f.b) && (f.a && f.b)))
+}
+
+class FSMOutputAlwaysOne(f: SimpleFSM) extends Spec(f) {
+  assert(f.b)
+}
+
+class FSMOutputAlwaysOneAssumingInputIsZero(f: SimpleFSM) extends Spec(f) {
+  assume(!f.a)
+  assert(f.b)
+}
 
 class StateMachineTest extends AnyFlatSpec with SymbiYosysTester {
   behavior of "SimpleFSM"
@@ -71,5 +83,23 @@ class StateMachineTest extends AnyFlatSpec with SymbiYosysTester {
       case VerificationSuccess =>
         assert(false, "expected this test to fail!")
     }
+  }
+
+  it should "ensure that a and b are never true for two consecutive cycles" in {
+    bmc(new SimpleFSM(), depth = 20, withSpec(new FSMInputOutputAssert(_))).throwErrors()
+  }
+
+  it should "allow the output to be 0" in {
+    bmc(new SimpleFSM(), 20, withSpec(new FSMOutputAlwaysOne(_))) match {
+      case v : VerificationFail =>
+        println(v.report)
+        assert(v.errors.size == 1, "expected exactly one error")
+      case VerificationSuccess =>
+        assert(false, "expected this test to fail!")
+    }
+  }
+
+  it should "always output one if the input is held low" in {
+    bmc(new SimpleFSM(), depth = 20, withSpec(new FSMOutputAlwaysOneAssumingInputIsZero(_))).throwErrors()
   }
 }
